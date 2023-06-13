41a90152d86fc306d0ee2d7fe471e53accfc7a5b
AlwaysWearSocks <105239898+AlwaysWearSocks@users.noreply.github.com>
2023-06-13 11:51:52 +0100

    Update streamlit_app.py
    Updated the API to work

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 # New section to display FruityVice requests
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
