784370a1943101ad75942a395011250049edb1c8
AlwaysWearSocks <105239898+AlwaysWearSocks@users.noreply.github.com>
2023-06-13 18:03:25 +0100

    Update streamlit_app.py
    Fixed a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 ## Snowflake Functions
 def get_fruit_load_list():
-    with mycnx.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
          return mycur.fetchall()
 
