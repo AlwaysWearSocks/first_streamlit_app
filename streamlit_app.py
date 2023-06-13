0676ee9e5354777d2f1411cb7e9480744c156194
AlwaysWearSocks <105239898+AlwaysWearSocks@users.noreply.github.com>
2023-06-13 11:57:09 +0100

    Update streamlit_app.py
    Updated to use a variable as the fruit data display

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # New section to display FruityVice requests
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalise the JSON response so it's not just text to a screen
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
