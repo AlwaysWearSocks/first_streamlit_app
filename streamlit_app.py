8610d6609c9213649995f287508e32b221bdbb15
AlwaysWearSocks <105239898+AlwaysWearSocks@users.noreply.github.com>
2023-06-13 18:04:36 +0100

    Update streamlit_app.py
    Fixed a further typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-         return mycur.fetchall()
+         return my_cur.fetchall()
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
