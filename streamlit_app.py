238d056a2b8e9b25cd7891e95ffcd9a0cf057321
AlwaysWearSocks <105239898+AlwaysWearSocks@users.noreply.github.com>
2023-06-13 17:28:44 +0100

    Update streamlit_app.py
    Tidied up the import commands

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import snowflake.connector
+import requests
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -25,7 +27,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 # New section to display FruityVice requests
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalise the JSON response so it's not just text to a screen
